928cb3b53ac813e70230da3a7304bee899f1a9da
azuki <448117151@qq.com>
2024-01-25 23:21:35 +0800

    3.9.7update

--- app/src/main/java/com/moe/moetranslator/geminiapi/ChatwithGemini.kt
+++ app/src/main/java/com/moe/moetranslator/geminiapi/ChatwithGemini.kt
@@ -25,9 +25,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.ai.client.generativeai.Chat
 import com.google.ai.client.generativeai.GenerativeModel
 import com.google.ai.client.generativeai.type.Content
+import com.google.ai.client.generativeai.type.ServerException
 import com.google.ai.client.generativeai.type.content
 import com.moe.moetranslator.R
-import com.moe.moetranslator.databinding.FragmentChatwithclaudeBinding
+import com.moe.moetranslator.databinding.FragmentChatwithgeminiBinding
 import com.moe.moetranslator.utils.MySharedPreferenceData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
@@ -36,7 +37,7 @@ import kotlinx.coroutines.launch
 
 class ChatwithGemini : Fragment() {
 
-    private lateinit var binding: FragmentChatwithclaudeBinding
+    private lateinit var binding: FragmentChatwithgeminiBinding
     private lateinit var repository: MySharedPreferenceData
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: MessageAdapter
@@ -56,7 +57,7 @@ class ChatwithGemini : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentChatwithclaudeBinding.inflate(inflater,container,false)
+        binding = FragmentChatwithgeminiBinding.inflate(inflater,container,false)
         return binding.root
     }
 
@@ -94,23 +95,31 @@ class ChatwithGemini : Fragment() {
                 binding.claudeInputBox.setText("")
                 if(repository.GeminiApi==""){
                     lifecycleScope.launch(Dispatchers.IO) {
-                        val response = "您还未配置Gemini API，请点击左上角齿轮按钮进行配置，也可在其中查看教程。"
+                        val response = "您还未配置Gemini API，请点击左上角齿轮按钮进行配置，也可在其中查看教程，配置完成后请先清除聊天记录再发送消息。"
                         val botMessage = ChatMessage(content = response, timestamp = System.currentTimeMillis(), sender = 1)
                         messageViewModel.insert(botMessage)
                     }
                 }else{
                     if(!(::geminimodel.isInitialized)){
-                        geminimodel = geminimodelfactory.createGeminiModel(repository.GeminiModel,repository.GeminiApi)
-                        val messages = messageViewModel.allMessages.value
-                        // 将每条消息转换为 Content 对象
-                        val chatHistory = mutableListOf<Content>()
-                        messages?.forEach { message ->
-                            val role = if (message.sender == 1) "model" else "user"
-                            chatHistory.add(content(role) { text(message.content) })
+                        try {
+                            geminimodel = geminimodelfactory.createGeminiModel(repository.GeminiModel,repository.GeminiApi)
+                            val messages = messageViewModel.allMessages.value
+                            // 将每条消息转换为 Content 对象
+                            val chatHistory = mutableListOf<Content>()
+                            messages?.forEach { message ->
+                                val role = if (message.sender == 1) "model" else "user"
+                                chatHistory.add(content(role) { text(message.content) })
+                            }
+                            geminichat = geminimodel.startChat(
+                                history = chatHistory
+                            )
+                        }catch (e: Exception){
+                            lifecycleScope.launch(Dispatchers.IO) {
+                                val response = "模型初始化失败，请检查Gemini API是否配置正确。"
+                                val botMessage = ChatMessage(content = response, timestamp = System.currentTimeMillis(), sender = 1)
+                                messageViewModel.insert(botMessage)
+                            }
                         }
-                        geminichat = geminimodel.startChat(
-                            history = chatHistory
-                        )
                     }
                     binding.buttonSend.text = "请等待"
                     binding.buttonSend.isClickable = false
@@ -120,20 +129,32 @@ class ChatwithGemini : Fragment() {
                         val currenttime = System.currentTimeMillis()
                         val botMessage = ChatMessage(content = response, timestamp = currenttime, sender = 1)
                         messageViewModel.insert(botMessage)
-                        geminichat.sendMessageStream(messageText).collect { chunk ->
-                            run {
-                                try {
-                                    messageViewModel.appendContentByTimestamp(
-                                        currenttime,
-                                        chunk.text!!
-                                    )
-                                } catch (e: Exception) {
-                                    messageViewModel.appendContentByTimestamp(
-                                        currenttime,
-                                        e.toString()
-                                    )
+                        try {
+                            geminichat.sendMessageStream(messageText).collect { chunk ->
+                                run {
+                                    try {
+                                        messageViewModel.appendContentByTimestamp(
+                                            currenttime,
+                                            chunk.text!!
+                                        )
+                                    } catch (e: Exception) {
+                                        messageViewModel.appendContentByTimestamp(
+                                            currenttime,
+                                            e.toString()
+                                        )
+                                    }
                                 }
                             }
+                        } catch (e: ServerException){
+                            messageViewModel.appendContentByTimestamp(
+                                currenttime,
+                                "服务器返回信息："+e.message.toString()
+                            )
+                        } catch (e: Exception){
+                            messageViewModel.appendContentByTimestamp(
+                                currenttime,
+                                "其他类型错误："+e.message.toString()+"\n开发者备注：大概率是API密钥含有中文或空格或其他非法字符导致的。"
+                            )
                         }
                         MainScope().launch {
                             binding.buttonSend.text = "发送"
@@ -159,7 +180,7 @@ class ChatwithGemini : Fragment() {
             textView.text = spannableString
 
             if(repository.GeminiApi!=""){
-                customView.findViewById<EditText>(R.id.gemini_api).hint = "（已保存）"
+                customView.findViewById<EditText>(R.id.gemini_api).hint = repository.GeminiApi+"（已保存）"
             }
             val mygeminiapi = customView.findViewById<EditText>(R.id.gemini_api)
             val myintr = customView.findViewById<TextView>(R.id.gemini_intr)
@@ -167,7 +188,7 @@ class ChatwithGemini : Fragment() {
             myintr.typeface = font
             val dialogBuilder = AlertDialog.Builder(context).setView(customView).setCancelable(false).setNegativeButton("取消"){_,_->}.setPositiveButton("保存"){_,_->
                 repository.saveGeminiApi(mygeminiapi.text.toString())
-                Toast.makeText(context,"保存成功", Toast.LENGTH_LONG).show()
+                Toast.makeText(context,"保存成功，请点击右上角清除聊天记录后再开始对话。", Toast.LENGTH_LONG).show()
             }.setNeutralButton("查看教程"){_,_->
                 val url = "https://blog.csdn.net/qq_45487246/article/details/135536624"
                 val intent = Intent(Intent.ACTION_VIEW)
