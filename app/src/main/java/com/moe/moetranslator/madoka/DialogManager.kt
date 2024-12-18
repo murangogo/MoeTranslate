e3f1c2c80fc276d3e616d2b540e7b5e3242ebcd3
azuki <448117151@qq.com>
2024-12-18 16:57:07 +0800

    live2d function icon optimization4.(Not Finish Yet) detail fix.

--- app/src/main/java/com/moe/moetranslator/madoka/DialogManager.kt
+++ app/src/main/java/com/moe/moetranslator/madoka/DialogManager.kt
@@ -23,7 +23,7 @@ class DialogManager private constructor() {
             dismissDialog() // 确保之前的 dialog 已经消失
 
             val title = dialogTitle ?: getApplicationContext().getString(R.string.loading)
-            val message = dialogMessage ?: getApplicationContext().getString(R.string.waiting)
+            val message = dialogMessage ?: getApplicationContext().getString(R.string.please_wait)
 
             // 使用 Handler 确保在主线程创建 dialog
             Handler(Looper.getMainLooper()).post {
